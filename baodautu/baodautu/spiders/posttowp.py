b9535c36c865c6ebc3d1708b618ea74b6839e17c
sinhduc01 <terragm01@gmail.com>
2022-10-03 09:17:53 +0700

    api

--- baodautu/baodautu/spiders/posttowp.py
+++ baodautu/baodautu/spiders/posttowp.py
@@ -1,4 +1,2 @@
-from wordpress_xmlrpc import Client, WordPressPost
-from wordpress_xmlrpc.methods.posts import GetPosts, NewPost
-from wordpress_xmlrpc.methods.users import GetUserInfo
-wp = Client('http://localhost/wordpress/xmlrpc.php', 'user', '123456')
+
+
